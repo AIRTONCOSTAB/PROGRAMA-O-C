c65c9ffb05d7bd56ffab1f807152a19bc2c56543
AIRTONCOSTAB <AIRTON.COSTAB@GMAIL.COM>
2024-11-20 17:05:13 -0300

    Atualizar o olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,13 @@
-#INCLUDE <
+#include <stdio.h>
+int main (){
+
+int idade = 25;
+float altura = 1.83;
+char letra = 'A';
+char nome[20] = "Airton"
+
+pintf("Idade: %d\nAltura: %f\nLetra: %c\nNome: %s");
+
+return 0;
+
+}
